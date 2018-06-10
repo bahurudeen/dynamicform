f8fd2b5088dd1de3032adf4e54f57f8bba5f95c5
Bahurudeen <bahurudeen@Bahurudeens-MacBook-Pro.local>
2018-06-10 22:11:25 +0400

    radio changes

--- src/app/components/radiobutton/radiobutton.component.ts
+++ src/app/components/radiobutton/radiobutton.component.ts
@@ -6,7 +6,7 @@ import { FieldConfig } from "../../field.interface";
   template: `
 <div class="demo-full-width margin-top" [formGroup]="group">
 <label class="radio-label-padding">{{field.label}}:</label>
-<mat-radio-group [formControlName]="field.name" [(ngModel)]="field.value">
+<mat-radio-group [formControlName]="field.name">
 <mat-radio-button *ngFor="let item of field.options" [value]="item">{{item}}</mat-radio-button>
 </mat-radio-group>
 </div>
